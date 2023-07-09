6ce4f99f11d1b151407bd545f5dd735782f48210
Mark <markwebdev@mail.ru>
2023-07-09 12:35:20 +0300

    Проект завершен

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
 window.addEventListener('DOMContentLoaded', () => {
+    // Меню
+    const menu = document.querySelector('.header__nav'),
+    menuButton = document.querySelector('.menu__button'),
+    closeMenuButton = document.querySelector('.close__button');
+
+    const openMenu = () => {
+        closeMenuButton.classList.add('active');
+        menu.classList.add('active');
+    };
+
+    const closeMenu = () => {
+        closeMenuButton.classList.remove('active');
+        menu.classList.remove('active');
+    }
+
+    menuButton.addEventListener('click', openMenu);
+    closeMenuButton.addEventListener('click', closeMenu);
+    // Меню
+
+    // Анимации
     const sectionAbout = document.querySelector('.section-about'),
-    sectionTop = document.querySelector('.section-top')
+    sectionTop = document.querySelector('.section-top'),
+    sectionMenu = document.querySelector('.section-menu'),
+    sectionSpecial = document.querySelector('.section-special'),
+    sectionChef = document.querySelector('.section-chef'),
+    sectionReviews = document.querySelector('.section-reviews'),
+    sectionRewards = document.querySelector('.section-rewards'),
+    sectionForm = document.querySelector('.section-form');
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
@@ -9,7 +35,23 @@ window.addEventListener('DOMContentLoaded', () => {
                 observer.unobserve(entry.target)
             }
         })
-    }, {threshold: 0.5});
+    }, {threshold: 0.4});
 
-   [sectionTop, sectionAbout].forEach((el) => observer.observe(el))
-})
+    [
+    sectionTop, sectionAbout, sectionSpecial,
+    sectionChef, sectionReviews, sectionRewards, sectionForm, sectionMenu
+    ]
+    .forEach((el) => observer.observe(el))
+    // Анимации
+
+    // Скролл по секциям
+    const anchorLinks = document.querySelectorAll('#anchor-link');
+
+    const scrollToSection = (sectionName) => {
+        document.querySelector(`.${sectionName}`).scrollIntoView({behavior: "smooth"});
+        closeMenu();
+    };
+
+    anchorLinks.forEach((el) => el.addEventListener('click', ({ target }) => scrollToSection(target.getAttribute('data-to'))))
+    // Скролл по секциям
+});
